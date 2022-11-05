157072a09c55d8503d003095eff9af63889f7817
jaygent <62799324+jaygent@users.noreply.github.com>
2022-11-05 15:05:43 +0100

    Update Request.php

--- system/Request.php
+++ system/Request.php
@@ -10,5 +10,6 @@ class Request
         $body=array_merge($_REQUEST,$_COOKIE,json_decode(file_get_contents("php://input"),1)??[],getallheaders());
         $this->body=new \ArrayObject($body);
         $this->body->setFlags(\ArrayObject::ARRAY_AS_PROPS);
+        return $this->body;
     }
 }
